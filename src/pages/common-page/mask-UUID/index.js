8b5b58b5accf9501c2a69bc331a211b4b30acee4
a18282607 <lexa2987@mail.ru>
2020-06-20 22:37:48 +0300

    add mask

--- src/pages/common-page/mask-UUID/index.js
+++ src/pages/common-page/mask-UUID/index.js
@@ -1,43 +1,15 @@
 import React, {useState, useRef} from 'react';
 import {Button} from "../../../components/button";
-import { delay } from '../../../helpers'
+import {delay, isEmpty} from '../../../helpers'
 import './style.css';
 
-const mask = "********-****-****-****-************";
+const mask = "_(__)_-__-__";
 
 export function MaskUUID(props) {
-    const [cursor, setCursor] = useState(false);
+    const [selectionStart, setSelectionStart] = useState(0);
+    const [prevMask, setPrevMask] = useState(mask);
+    const [symbol, setSymbol] = useState('');
     const inputRef = useRef({});
-
-    function mask(e) {
-        // setCursor(e.target.selectionStart);
-        // setCaretPosition(e.target, e.target.selectionStart, e.target.selectionStart)
-        const matrix = "+3**(**)****-***";
-        let i = 0;
-        let val = e.target.value;
-        if (val === '3') {
-            val = e.target.value.replace(/[+()-]/g, "");
-        } else {
-            val = e.target.value.replace(/[+()-]/g, "").replace(/[3]/, '');
-        }
-        const start = e.target.selectionStart;
-        e.target.value = matrix.replace(/[\s\S]/g, function (a) {
-
-            if (/[*]/.test(a) && i < val.length) {
-                return val.charAt(i++)
-            } else {
-                return i >= val.length ? "_" : a
-            }
-
-        });
-
-        if (e.target.value.length === matrix.length) {
-            setCaretPosition(inputRef.current, start, start)
-        } else if( start < matrix.length) {
-            //delay(0).then(() => setCaretPosition(inputRef.current, start+ 1, start+ 1))
-        }
-
-    }
 
     function setCaretPosition(ctrl, start, end) {
         if (ctrl.setSelectionRange) {
@@ -46,8 +18,28 @@ export function MaskUUID(props) {
         }
     }
 
+    function maskReplace(e) {
+        let paste = (e.clipboardData || window.clipboardData).getData('text');
+        const selection = window.getSelection();
+        if (!selection.rangeCount) return false;
+        const matrix = mask;
+        let i = 0;
+        let val = paste;
+        val = val.replace(/[-]/g, "");
 
+        let r = matrix.replace(/[\s\S]/g, function (a) {
 
+            if (/[_]/.test(a) && i < val.length) {
+                return val.charAt(i++)
+            } else {
+                return i >= val.length ? "_" : a
+            }
+
+        });
+        delay(0)
+            .then(() => inputRef.current.value = r)
+            .then(() => setPrevMask(r));
+    }
 
     return (
         <div>
@@ -55,18 +47,69 @@ export function MaskUUID(props) {
                 type="text"
                 className="mask-uuid"
                 onChange={(e) => {
-                    const value = e.target.value;
-                    mask(e)
 
                 }}
-                ref={inputRef}
-                onKeyDown={e => {
-                    const cur = e.target.selectionStart;
-                    if (e.keyCode === 8) {
-                        delay(0).then(() => setCaretPosition(inputRef.current, cur - 1, cur - 1));
+                onInput={e => {
+                    const maskSymbol = mask.slice(selectionStart, selectionStart + 1);
+                    const nextMaskSymbol = mask.slice(selectionStart - 1, selectionStart);
 
-
+                    if (selectionStart === mask.length && symbol !== '_') {
+                        e.target.value = prevMask;
+                        return;
                     }
+                    if (isEmpty(symbol)) {
+                        e.target.value = prevMask;
+                        setCaretPosition(e.target, selectionStart, selectionStart);
+                        return
+                    }
+                    if (symbol === '_' && nextMaskSymbol === '-') {
+                        e.target.value = prevMask;
+                        setCaretPosition(e.target, selectionStart - 1, selectionStart - 1);
+                        return;
+                    }
+                    if (symbol === '_') {
+                        const updateMask = prevMask.slice(0, selectionStart - 1) + symbol + prevMask.slice(selectionStart, prevMask.length);
+                        setPrevMask(updateMask);
+                        e.target.value = updateMask;
+                        setCaretPosition(e.target, selectionStart - 1, selectionStart - 1);
+                        return;
+                    }
+                    if (maskSymbol === '_') {
+                        const updateMask = prevMask.slice(0, selectionStart) + symbol + prevMask.slice(selectionStart + 1, prevMask.length);
+                        setPrevMask(updateMask);
+                        e.target.value = updateMask;
+                        setCaretPosition(e.target, selectionStart + 1, selectionStart + 1)
+                    } else {
+                        const updateMask = prevMask.slice(0, selectionStart) + maskSymbol + symbol + prevMask.slice(selectionStart + 2, prevMask.length);
+                        setPrevMask(updateMask);
+                        e.target.value = updateMask;
+                        setCaretPosition(e.target, selectionStart + 2, selectionStart + 2)
+                    }
+                }}
+                ref={inputRef}
+                onKeyDown={(e) => {
+
+                    if (e.key.length === 1 && /[\w]/.test(e.key)) {
+                        setSymbol(e.key);
+                    } else {
+                        setSymbol(null)
+                    }
+                    delay(0).then(() => setSelectionStart(inputRef.current.selectionStart));
+                    if (e.keyCode === 8) {
+                        setSymbol('_')
+                    }
+                }}
+                onFocus={e => {
+                    if (isEmpty(e.target.value)) {
+                        e.target.value = mask
+                    }
+
+                }}
+                onMouseDown={e => {
+                    delay(0).then(() => setSelectionStart(inputRef.current.selectionStart));
+                }}
+                onPaste={e => {
+                    maskReplace(e)
                 }}
 
             />
